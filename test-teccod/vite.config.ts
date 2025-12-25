486b0a2625df9e0b04c96778c51f4b8e70147c37
Alimpos <alimpostea@mail.ru>
2025-12-26 02:26:46 +0300

    refactor: edited vite.config.ts

--- test-teccod/vite.config.ts
+++ test-teccod/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { Server } from 'https'
-import { start } from 'repl'
 
 // https://vite.dev/config/
 export default defineConfig({
